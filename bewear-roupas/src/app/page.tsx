f4013dde39a7af3e4d677414620486ed7e737b17
adadezer <adadezer@gmail.com>
2025-08-05 12:19:27 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ bewear-roupas/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button className="bg-red-500 px-5">Bootcamp</Button>
+    </div>
+  );
+};
+
+export default Home;
