9164dfb6a4397a76e2f6d306fb00e1f8fc3210d1
adadezer <adadezer@gmail.com>
2025-08-06 19:00:43 -0300

    feat: add sign out

--- bewear-roupas/src/app/authentication/page.tsx
+++ bewear-roupas/src/app/authentication/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SingInForm from "./components/sign-in-form";
@@ -7,22 +8,25 @@ import SingUpForm from "./components/sign-up-form";
 
 const Authentication = () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
-        </TabsList>
+    <>
+      <Header />
+      <div className="flex w-full flex-col gap-6 p-5">
+        <Tabs defaultValue="sign-in">
+          <TabsList className="w-full">
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="sign-in">
-          <SingInForm />
-        </TabsContent>
+          <TabsContent value="sign-in">
+            <SingInForm />
+          </TabsContent>
 
-        <TabsContent value="sign-up">
-          <SingUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+          <TabsContent value="sign-up">
+            <SingUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
