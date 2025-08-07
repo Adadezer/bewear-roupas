2efab99fbd7c5ba62a6026edb5c87b2ced8be817
adadezer <adadezer@gmail.com>
2025-08-07 11:37:44 -0300

    feat: add product list and product item components

--- bewear-roupas/src/app/page.tsx
+++ bewear-roupas/src/app/page.tsx
@@ -1,11 +1,41 @@
+import Image from "next/image";
 import React from "react";
 
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full px-5"
+        />
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <Image
+          src="/banner-02.png"
+          alt="seja autentico"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full px-5"
+        />
+      </div>
     </>
   );
 };
